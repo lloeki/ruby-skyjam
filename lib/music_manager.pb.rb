d5392e97baed18230c5ed16e9dfd1c8102443d78
Loic Nageleisen <loic.nageleisen@gmail.com>
2014-05-30 11:54:48 +0200

    regenerated

--- lib/music_manager.pb.rb
+++ lib/music_manager.pb.rb
@@ -13,19 +13,22 @@ module MusicManager
       define :ALL, 1
       define :STORE, 2
     end
-    
+
   end
+
   class ExportTracksResponse < ::Protobuf::Message
     class Status < ::Protobuf::Enum
       define :OK, 1
       define :TRANSIENT_ERROR, 2
-      define :CLIENT_MAXED, 3
+      define :MAX_CLIENTS, 3
       define :CLIENT_AUTH_ERROR, 4
       define :CLIENT_REG_ERROR, 5
     end
-    
+
     class TrackInfo < ::Protobuf::Message; end
+
   end
+
   class AuthRequest < ::Protobuf::Message; end
   class Response < ::Protobuf::Message
     class Type < ::Protobuf::Enum
@@ -39,7 +42,7 @@ module MusicManager
       define :UPDATE_UPLOAD_STATE, 8
       define :DELETE_UPLOAD_REQUESTED, 9
     end
-    
+
     class AuthStatus < ::Protobuf::Enum
       define :OK, 8
       define :MAX_LIMIT_REACHED, 9
@@ -50,7 +53,7 @@ module MusicManager
       define :NOT_SUBSCRIBED, 14
       define :INVALID_REQUEST, 15
     end
-    
+
     class Status < ::Protobuf::Message
       class Code < ::Protobuf::Enum
         define :OK, 1
@@ -58,10 +61,14 @@ module MusicManager
         define :SOFT_ERROR, 3
         define :METADATA_TOO_LARGE, 4
       end
-      
+
     end
+
+
   end
-  
+
+
+
   ##
   # Message Fields
   #
@@ -71,7 +78,7 @@ module MusicManager
     optional ::MusicManager::ExportTracksRequest::TrackType, :export_type, 4
     optional ::Protobuf::Field::Int64Field, :updated_min, 5
   end
-  
+
   class ExportTracksResponse
     class TrackInfo
       optional ::Protobuf::Field::StringField, :id, 1
@@ -82,28 +89,27 @@ module MusicManager
       optional ::Protobuf::Field::Int32Field, :track_number, 6
       optional ::Protobuf::Field::Int64Field, :track_size, 7
     end
-    
+
     required ::MusicManager::ExportTracksResponse::Status, :status, 1
     repeated ::MusicManager::ExportTracksResponse::TrackInfo, :track_info, 2
     optional ::Protobuf::Field::StringField, :continuation_token, 3
     optional ::Protobuf::Field::Int64Field, :updated_min, 4
   end
-  
+
   class AuthRequest
     required ::Protobuf::Field::StringField, :id, 1
     optional ::Protobuf::Field::StringField, :name, 2
   end
-  
+
   class Response
     class Status
       required ::MusicManager::Response::Status::Code, :code, 1
     end
-    
+
     optional ::MusicManager::Response::Type, :type, 1
     optional ::MusicManager::Response::AuthStatus, :auth_status, 11
     optional ::Protobuf::Field::BoolField, :auth_error, 12
   end
-  
-  
+
 end
 
